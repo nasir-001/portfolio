dd74a95034c0aacdeb7a19438a516524bd4e7448
Nasir Lawal <nasirlawal001@gmail.com>
2022-07-19 09:47:05 +0100

    add about me section

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    About me
+  <div class="tw-mt-80 tw-px-5 lg:tw-pl-12 xl:tw-pl-24">
+    <div class="row">
+      <div class="tw-px-10">
+        <span class="tw-text-2xl tw-font-light tw-text-primary-text-color">01.</span><span class="tw-text-4xl tw-pl-4 tw-text-gray-300">About Me</span>
+      </div>
+      <div class="tw-border-t tw-mt-5 tw-border-gray-700 tw-w-80"></div>
+    </div>
   </div>
 </template>
 
