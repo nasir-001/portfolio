5e8c004e21ad1c16b2d3544063ba7a459a7a5635
Nasir Lawal <nasirlawal001@gmail.com>
2022-08-08 12:45:18 +0100

    design projects card

--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -6,6 +6,27 @@
       </div>
       <div class="tw-border-t tw-mt-5 tw-mx-auto sm:tw-mx-0 tw-border-gray-700 tw-w-80"></div>
     </div>
+    <div class="tw-flex tw-justify-center tw-mt-16">
+      <div class="tw-w-80 tw-bg-projects-bg-color tw-h-80 tw-rounded-md">
+        <div class="tw-flex tw-justify-end tw-m-4">
+          <a href="https://github.com/nasir-001/Django-Student-Helpline-Portal" target="_blank" rel="noopener noreferrer"> 
+            <q-icon name="fa-brands fa-github" class="tw-mx-2 tw-mt-3 tw-text-primary-text-color" size="sm" />
+          </a>
+          <a href="https://student-helpline.heroku.app" target="_blank" rel="noopener noreferrer">
+            <q-icon name="link" class="tw-mx-2 tw-mt-3 tw-text-primary-text-color" size="sm" />
+          </a>
+        </div>
+        <div class="tw-pl-4 tw-text-gray-300 tw-text-xl tw-font-semibold">
+          Helpline portal
+        </div>
+        <div class="tw-h-44 tw-px-4 tw-pt-4 tw-text-gray-400 tw-text-lg tw-font-light">
+          A web application that provides a room for discussion between lecturers and students in a university.
+        </div>
+        <div class="tw-font-mono tw-px-4 tw-text-gray-400">
+          Django, jQuery. Bootstrap, CSS
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
