7cb93c6ab4116c1895a44dd114e33e6a254a0fa7
Nasir Lawal <nasirlawal001@gmail.com>
2022-08-08 11:19:38 +0100

    add responsive to the contact section

--- src/components/GetInTouch.vue
+++ src/components/GetInTouch.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="tw-text-center tw-pt-36">
     <span class="tw-text-xl tw-font-light tw-text-primary-text-color">04. What is next?</span>
-    <div class="tw-text-gray-300 tw-pt-4 tw-text-6xl">
+    <div class="tw-text-gray-300 tw-pt-4 tw-text-4xl sm:tw-text-6xl">
       Get in Touch
     </div>
     <div class="tw-text-gray-400 tw-text-base tw-font-light sm:tw-text-xl tw-pt-10">
-      I'm currently looking for any new opportunities, my inbox is <br>
-      always open. Whether you have any job for me or just <br>
-      want to say hi, I'll definitely get back to you!
+      I'm currently looking for any new opportunities, my <br class="sm:tw-hidden"> inbox is <br class="tw-hidden sm:tw-block">
+      always open. Whether you have any job <br class="sm:tw-hidden"> for me or just <br class="tw-hidden sm:tw-block">
+      want to say hi, I'll definitely <br class="sm:tw-hidden"> get back to you!
     </div>
     <div class="tw-mt-12 tw-rounded-md tw-mx-auto tw-w-44 tw-h-16 tw-p-0.5 tw-border-2 tw-border-primary-text-color">
       <a href="mailto:nasirlawal001@gmail.com">
