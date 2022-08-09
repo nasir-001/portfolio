627020d474b40459fecd049939de6534b5d030e7
Nasir Lawal <nasirlawal001@gmail.com>
2022-08-09 01:31:01 +0100

    fixed get in touch section

--- src/components/GetInTouch.vue
+++ src/components/GetInTouch.vue
@@ -5,9 +5,9 @@
       Get in Touch
     </div>
     <div class="tw-text-gray-400 tw-text-base tw-font-light sm:tw-text-xl tw-pt-10">
-      I'm currently looking for any new opportunities, my <br class="sm:tw-hidden"> inbox is <br class="tw-hidden sm:tw-block">
-      always open. Whether you have any job <br class="sm:tw-hidden"> for me or just <br class="tw-hidden sm:tw-block">
-      want to say hi, I'll definitely <br class="sm:tw-hidden"> get back to you!
+      I'm currently looking for any new opportunities, <br class="sm:tw-hidden"> my inbox is <br class="tw-hidden sm:tw-block">
+      always open. Whether you have any <br class="sm:tw-hidden"> job for me or just <br class="tw-hidden sm:tw-block">
+      want to say hi, I'll <br class="sm:tw-hidden"> definitely get back to you!
     </div>
     <div class="tw-mt-12 tw-rounded-md tw-mx-auto tw-w-44 tw-h-16 tw-p-0.5 tw-border-2 tw-border-primary-text-color">
       <a href="mailto:nasirlawal001@gmail.com">
