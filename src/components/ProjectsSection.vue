7d9e9b1320a0a2f848a1733048b3bc9c0befccf7
Nasir Lawal <nasirlawal001@gmail.com>
2022-08-13 16:51:56 +0100

    change experience section

--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -99,105 +99,117 @@
       Other Noteworthy Projects
     </div>
     <div class="tw-flex tw-flex-wrap sm:tw-flex-nowrap -tw-ml-4 sm:tw-mr-3 lg:tw-mr-0 tw-justify-center tw-mt-5">
-      <div data-aos="zoom-in" class="tw-w-96 lg:tw-w-80 tw-bg-projects-bg-color tw-h-80 tw-rounded-md shadow-9 tw-mt-6 tw-mx-1 lg:tw-mx-3 step hover:tw-border-2 hover:tw-border-primary-text-color">
-        <div class="tw-flex tw-justify-end tw-m-4">
-          <a href="https://github.com/nasir-001/Django-Student-Helpline-Portal" target="_blank" rel="noopener noreferrer"> 
-            <q-icon name="fa-brands fa-github" class="tw-mx-2 tw-mt-3 tw-text-primary-text-color" size="sm" />
-          </a>
-          <a href="https://student-helpline.heroku.app" target="_blank" rel="noopener noreferrer">
-            <q-icon name="launch" class="tw-mx-2 tw-mt-3 tw-text-primary-text-color" size="sm" />
-          </a>
-        </div>
-        <div class="tw-pl-4 tw-text-gray-300 tw-text-xl tw-font-semibold">
-          Helpline portal
-        </div>
-        <div class="tw-h-44 tw-px-4 tw-pt-4 tw-text-gray-400 tw-text-sm lg:tw-text-lg tw-font-light">
-          A web application that provides a room for discussion between lecturers and students in a university.
-        </div>
-        <div class="tw-font-mono tw-text-xs tw-px-4 tw-text-gray-400">
-          Django, jQuery, Bootstrap, CSS
-        </div>
-      </div>
-      <div data-aos="zoom-in" class="tw-w-96 lg:tw-w-80 tw-bg-projects-bg-color tw-h-80 tw-rounded-md shadow-9 tw-mt-6 tw-mx-1 lg:tw-mx-3 step hover:tw-border-2 hover:tw-border-primary-text-color">
-        <div class="tw-flex tw-justify-end tw-m-4">
-          <a href="https://github.com/nasir-001/Django-online-employent-system" target="_blank" rel="noopener noreferrer"> 
-            <q-icon name="fa-brands fa-github" class="tw-mx-2 tw-mt-3 tw-text-primary-text-color" size="sm" />
-          </a>
-        </div>
-        <div class="tw-pl-4 tw-text-gray-300 tw-text-xl tw-font-semibold">
-          Job management
-        </div>
-        <div class="tw-h-44 tw-px-4 tw-pt-4 tw-text-gray-400 tw-text-sm lg:tw-text-lg tw-font-light">
-          A django application where employers can post a vacancy and candidates can and upload resume, a successful candidate will recieve a mail.
-        </div>
-        <div class="tw-font-mono tw-text-xs tw-px-4 tw-text-gray-400">
-          Django, Bootstrap, CSS, SCSS
+      <div data-aos="zoom-in">
+        <div class="tw-w-96 lg:tw-w-80 tw-bg-projects-bg-color tw-h-80 tw-rounded-md shadow-9 tw-mt-6 tw-mx-1 lg:tw-mx-3 step hover:tw-border-2 hover:tw-border-primary-text-color">
+          <div class="tw-flex tw-justify-end tw-m-4">
+            <a href="https://github.com/nasir-001/Django-Student-Helpline-Portal" target="_blank" rel="noopener noreferrer"> 
+              <q-icon name="fa-brands fa-github" class="tw-mx-2 tw-mt-3 tw-text-primary-text-color" size="sm" />
+            </a>
+            <a href="https://student-helpline.heroku.app" target="_blank" rel="noopener noreferrer">
+              <q-icon name="launch" class="tw-mx-2 tw-mt-3 tw-text-primary-text-color" size="sm" />
+            </a>
+          </div>
+          <div class="tw-pl-4 tw-text-gray-300 tw-text-xl tw-font-semibold">
+            Helpline portal
+          </div>
+          <div class="tw-h-44 tw-px-4 tw-pt-4 tw-text-gray-400 tw-text-sm lg:tw-text-lg tw-font-light">
+            A web application that provides a room for discussion between lecturers and students in a university.
+          </div>
+          <div class="tw-font-mono tw-text-xs tw-px-4 tw-text-gray-400">
+            Django, jQuery, Bootstrap, CSS
+          </div>
         </div>
       </div>
-      <div data-aos="zoom-in" class="tw-w-96 lg:tw-w-80 tw-bg-projects-bg-color tw-h-80 tw-rounded-md shadow-9 tw-mt-6 tw-mx-1 lg:tw-mx-3 step hover:tw-border-2 hover:tw-border-primary-text-color">
-        <div class="tw-flex tw-justify-end tw-m-4">
-          <a href="https://github.com/nasir-001/attendance-sys-ui" target="_blank" rel="noopener noreferrer"> 
-            <q-icon name="fa-brands fa-github" class="tw-mx-2 tw-mt-3 tw-text-primary-text-color" size="sm" />
-          </a>
+      <div data-aos="zoom-in">
+        <div class="tw-w-96 lg:tw-w-80 tw-bg-projects-bg-color tw-h-80 tw-rounded-md shadow-9 tw-mt-6 tw-mx-1 lg:tw-mx-3 step hover:tw-border-2 hover:tw-border-primary-text-color">
+          <div class="tw-flex tw-justify-end tw-m-4">
+            <a href="https://github.com/nasir-001/Django-online-employent-system" target="_blank" rel="noopener noreferrer"> 
+              <q-icon name="fa-brands fa-github" class="tw-mx-2 tw-mt-3 tw-text-primary-text-color" size="sm" />
+            </a>
+          </div>
+          <div class="tw-pl-4 tw-text-gray-300 tw-text-xl tw-font-semibold">
+            Job management
+          </div>
+          <div class="tw-h-44 tw-px-4 tw-pt-4 tw-text-gray-400 tw-text-sm lg:tw-text-lg tw-font-light">
+            A django application where employers can post a vacancy and candidates can and upload resume, a successful candidate will recieve a mail.
+          </div>
+          <div class="tw-font-mono tw-text-xs tw-px-4 tw-text-gray-400">
+            Django, Bootstrap, CSS, SCSS
+          </div>
         </div>
-        <div class="tw-pl-4 tw-text-gray-300 tw-text-xl tw-font-semibold">
-          Attendance system
-        </div>
-        <div class="tw-h-44 tw-px-4 tw-pt-4 tw-text-gray-400 tw-text-sm lg:tw-text-lg tw-font-light">
-          An online attendance system which helps in keeping record of attendees in an events for future reference.
-        </div>
-        <div class="tw-font-mono tw-text-xs tw-px-4 tw-text-gray-400">
-          FastAPI, Vuejs, Quasar, Tailwindcss
+      </div>
+      <div data-aos="zoom-in">
+        <div class="tw-w-96 lg:tw-w-80 tw-bg-projects-bg-color tw-h-80 tw-rounded-md shadow-9 tw-mt-6 tw-mx-1 lg:tw-mx-3 step hover:tw-border-2 hover:tw-border-primary-text-color">
+          <div class="tw-flex tw-justify-end tw-m-4">
+            <a href="https://github.com/nasir-001/attendance-sys-ui" target="_blank" rel="noopener noreferrer"> 
+              <q-icon name="fa-brands fa-github" class="tw-mx-2 tw-mt-3 tw-text-primary-text-color" size="sm" />
+            </a>
+          </div>
+          <div class="tw-pl-4 tw-text-gray-300 tw-text-xl tw-font-semibold">
+            Attendance system
+          </div>
+          <div class="tw-h-44 tw-px-4 tw-pt-4 tw-text-gray-400 tw-text-sm lg:tw-text-lg tw-font-light">
+            An online attendance system which helps in keeping record of attendees in an events for future reference.
+          </div>
+          <div class="tw-font-mono tw-text-xs tw-px-4 tw-text-gray-400">
+            FastAPI, Vuejs, Quasar, Tailwindcss
+          </div>
         </div>
       </div>
     </div>
     <div class="tw-flex -tw-ml-4  sm:tw-mr-3 lg:tw-mr-0 tw-justify-center tw-flex-wrap sm:tw-flex-nowrap sm:tw-mt-8">
-      <div data-aos="zoom-in" class="tw-w-96 lg:tw-w-80 tw-bg-projects-bg-color tw-h-80 tw-rounded-md shadow-9 tw-mt-6 tw-mx-1 lg:tw-mx-3 step hover:tw-border-2 hover:tw-border-primary-text-color">
-        <div class="tw-flex tw-justify-end tw-m-4">
-          <a href="https://github.com/nasir-001/Django-Student-Helpline-Portal" target="_blank" rel="noopener noreferrer"> 
-            <q-icon name="fa-brands fa-github" class="tw-mx-2 tw-mt-3 tw-text-primary-text-color" size="sm" />
-          </a>
-        </div>
-        <div class="tw-pl-4 tw-text-gray-300 tw-text-xl tw-font-semibold">
-          Admission system
-        </div>
-        <div class="tw-h-44 tw-px-4 tw-pt-4 tw-text-gray-400 tw-text-sm lg:tw-text-lg tw-font-light">
-          An online web system that takes student with higher entry exam scores and admit them.
-        </div>
-        <div class="tw-font-mono tw-text-xs tw-px-4 tw-text-gray-400">
-          Django, Bootstrap, CSS
-        </div>
-      </div>
-      <div data-aos="zoom-in" class="tw-w-96 lg:tw-w-80 tw-bg-projects-bg-color tw-h-80 tw-rounded-md shadow-9 tw-mt-6 tw-mx-1 lg:tw-mx-3 step hover:tw-border-2 hover:tw-border-primary-text-color">
-        <div class="tw-flex tw-justify-end tw-m-4">
-          <a href="https://github.com/nasir-001/E-Commerce" target="_blank" rel="noopener noreferrer"> 
-            <q-icon name="fa-brands fa-github" class="tw-mx-2 tw-mt-3 tw-text-primary-text-color" size="sm" />
-          </a>
-        </div>
-        <div class="tw-pl-4 tw-text-gray-300 tw-text-xl tw-font-semibold">
-          Shopeit
-        </div>
-        <div class="tw-h-44 tw-px-4 tw-pt-4 tw-text-gray-400 tw-text-sm lg:tw-text-lg tw-font-light">
-          An e-commerce application with online payments feature and google maps for driver to help in delivery.
-        </div>
-        <div class="tw-font-mono tw-text-xs tw-px-4 tw-text-gray-400">
-          Laravel, Orchid UI, Vuejs, Tailwind
+      <div data-aos="zoom-in">
+        <div class="tw-w-96 lg:tw-w-80 tw-bg-projects-bg-color tw-h-80 tw-rounded-md shadow-9 tw-mt-6 tw-mx-1 lg:tw-mx-3 step hover:tw-border-2 hover:tw-border-primary-text-color">
+          <div class="tw-flex tw-justify-end tw-m-4">
+            <a href="https://github.com/nasir-001/Django-Student-Helpline-Portal" target="_blank" rel="noopener noreferrer"> 
+              <q-icon name="fa-brands fa-github" class="tw-mx-2 tw-mt-3 tw-text-primary-text-color" size="sm" />
+            </a>
+          </div>
+          <div class="tw-pl-4 tw-text-gray-300 tw-text-xl tw-font-semibold">
+            Admission system
+          </div>
+          <div class="tw-h-44 tw-px-4 tw-pt-4 tw-text-gray-400 tw-text-sm lg:tw-text-lg tw-font-light">
+            An online web system that takes student with higher entry exam scores and admit them.
+          </div>
+          <div class="tw-font-mono tw-text-xs tw-px-4 tw-text-gray-400">
+            Django, Bootstrap, CSS
+          </div>
         </div>
       </div>
-      <div data-aos="zoom-in" class="tw-w-96 lg:tw-w-80 tw-bg-projects-bg-color tw-h-80 tw-rounded-md shadow-9 tw-mt-6 tw-mx-1 lg:tw-mx-3 step hover:tw-border-2 hover:tw-border-primary-text-color">
-        <div class="tw-flex tw-justify-end tw-m-4">
-          <a href="https://github.com/nasir-001/flutter_todo_app" target="_blank" rel="noopener noreferrer"> 
-            <q-icon name="fa-brands fa-github" class="tw-mx-2 tw-mt-3 tw-text-primary-text-color" size="sm" />
-          </a>
+      <div data-aos="zoom-in">
+        <div class="tw-w-96 lg:tw-w-80 tw-bg-projects-bg-color tw-h-80 tw-rounded-md shadow-9 tw-mt-6 tw-mx-1 lg:tw-mx-3 step hover:tw-border-2 hover:tw-border-primary-text-color">
+          <div class="tw-flex tw-justify-end tw-m-4">
+            <a href="https://github.com/nasir-001/E-Commerce" target="_blank" rel="noopener noreferrer"> 
+              <q-icon name="fa-brands fa-github" class="tw-mx-2 tw-mt-3 tw-text-primary-text-color" size="sm" />
+            </a>
+          </div>
+          <div class="tw-pl-4 tw-text-gray-300 tw-text-xl tw-font-semibold">
+            Shopeit
+          </div>
+          <div class="tw-h-44 tw-px-4 tw-pt-4 tw-text-gray-400 tw-text-sm lg:tw-text-lg tw-font-light">
+            An e-commerce application with online payments feature and google maps for driver to help in delivery.
+          </div>
+          <div class="tw-font-mono tw-text-xs tw-px-4 tw-text-gray-400">
+            Laravel, Orchid UI, Vuejs, Tailwind
+          </div>
         </div>
-        <div class="tw-pl-4 tw-text-gray-300 tw-text-xl tw-font-semibold">
-          Todo app
-        </div>
-        <div class="tw-h-44 tw-px-4 tw-pt-4 tw-text-gray-400 tw-text-sm lg:tw-text-lg tw-font-light">
-          A mobile app simple todo-list which uses calendar to schedule a task and a notification reminder for each task.
-        </div>
-        <div class="tw-font-mono tw-text-xs tw-px-4 tw-text-gray-400">
-          Flutter, date-picker, google fonts
+      </div>
+      <div data-aos="zoom-in">
+        <div class="tw-w-96 lg:tw-w-80 tw-bg-projects-bg-color tw-h-80 tw-rounded-md shadow-9 tw-mt-6 tw-mx-1 lg:tw-mx-3 step hover:tw-border-2 hover:tw-border-primary-text-color">
+          <div class="tw-flex tw-justify-end tw-m-4">
+            <a href="https://github.com/nasir-001/flutter_todo_app" target="_blank" rel="noopener noreferrer"> 
+              <q-icon name="fa-brands fa-github" class="tw-mx-2 tw-mt-3 tw-text-primary-text-color" size="sm" />
+            </a>
+          </div>
+          <div class="tw-pl-4 tw-text-gray-300 tw-text-xl tw-font-semibold">
+            Todo app
+          </div>
+          <div class="tw-h-44 tw-px-4 tw-pt-4 tw-text-gray-400 tw-text-sm lg:tw-text-lg tw-font-light">
+            A mobile app simple todo-list which uses calendar to schedule a task and a notification reminder for each task.
+          </div>
+          <div class="tw-font-mono tw-text-xs tw-px-4 tw-text-gray-400">
+            Flutter, date-picker, google fonts
+          </div>
         </div>
       </div>
     </div>
